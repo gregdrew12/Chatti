a02ac070af341d4dd674ee08f1efd5e72303f249
gregdrew12 <gregdrew12@gmail.com>
2023-08-25 16:07:53 -0400

    Minor changes

--- chatti-fe/webpack.config.js
+++ chatti-fe/webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
                exclude: /node_modules/,
             },
             {
-            //   exclude: /node_modules/,
+               exclude: /node_modules/,
               test: /\.css$/i,
                 use: [
                   "style-loader",
