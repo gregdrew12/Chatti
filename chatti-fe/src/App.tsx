eb806c4d9e89c9b5d9644091f40e7446b092151b
gregdrew12 <gregdrew12@gmail.com>
2023-08-26 12:50:24 -0400

    Database connected and functioning

--- chatti-fe/src/App.tsx
+++ chatti-fe/src/App.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import "./App.css"
 
 function App() {
+
+  const [url, setUrl] = useState('');
+
+  useEffect(() => {
+    chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+      let url = tabs[0].url;
+      if(typeof url === 'string')
+      {
+        setUrl(url);
+      }
+      
+  });
+  }, []);
+
   return (
     <div className="App" style={{color: 'red'}}>
-      Welcome to Chatti!
+      Welcome to Chatti! <br/>
+      The current URL is {url}
     </div>
   );
 }
