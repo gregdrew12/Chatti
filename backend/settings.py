a080bfbf031b1ca6ca45f5b6fda1f942ac5bcc49
gregdrew12 <gregdrew12@gmail.com>
2023-08-28 17:18:42 -0400

    update allowed_hosts

--- backend/settings.py
+++ backend/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '24.199.80.26'
+]
 
 
 # Application definition
